55d1ad381387aa2f096811984d7fcccdfe010ca7
Migue Garcia <miguelgarciabarrera@tamu.edu>
2018-11-15 09:41:11 -0600

    TX from zybo working

--- UART_v1/xuartps_polled_example.c
+++ UART_v1/xuartps_polled_example.c
@@ -1,300 +1,320 @@
-/******************************************************************************/
-/*                                                                            */
-/* main.c -- Program to drive 2 dual channel H-bridges (i.e. 4 motors)        */
-/*                                                                            */
-/*                                                                            */
-/******************************************************************************/
-/* File Description:                                                          */
-/*                                                                            */
-/* This program drives 4 motors in the 4 possible directions using tank       */
-/* steering.                                                                  */
-/******************************************************************************/
+/******************************************************************************
+*
+* Copyright (C) 2010 - 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/****************************************************************************/
+/**
+*
+* @file     xuartps_polled_example.c
+*
+* This file contains an example using the XUartPs driver in polled mode.
+*
+* This function sends data and expects to receive the data thru the device
+* using the local loopback mode.
+*
+* @note
+* If the device does not work properly, the example may hang.
+*
+* MODIFICATION HISTORY:
+* <pre>
+* Ver   Who    Date     Changes
+* ----- ------ -------- -----------------------------------------------
+* 1.00a  drg/jz 01/13/10 First Release
+* 1.03a  sg     07/16/12 Modified the device ID to use the first Device Id
+*			Removed the printf at the start of the main
+* </pre>
+******************************************************************************/
 
-#include "PmodDHB1.h"
-#include "PWM.h"
-#include "sleep.h"
-#include "xil_cache.h"
+/***************************** Include Files *********************************/
+
 #include "xparameters.h"
-
-// headers for GPIO
-#include "xil_io.h"
-#include "sleep.h"
-#include <xgpio.h>
-#include "platform.h"
-#include <stdio.h>
-
-// headers for UART
 #include "xuartps.h"
 #include "xil_printf.h"
 
+/************************** Constant Definitions *****************************/
 
-/************ Macro Definitions ************/
-
-//GPIO control
-#define GPIO_BASEADDR     XPAR_PMODDHB1_0_AXI_LITE_GPIO_BASEADDR
-
-//PMOD JD
-#define JD_GPIO_BASEADDR     XPAR_PMODDHB1_0_AXI_LITE_GPIO_BASEADDR
-#define JD_PWM_BASEADDR      XPAR_PMODDHB1_0_PWM_AXI_BASEADDR
-
-//PMOD JE
-#define JE_GPIO_BASEADDR     XPAR_PMODDHB1_1_AXI_LITE_GPIO_BASEADDR
-#define JE_PWM_BASEADDR      XPAR_PMODDHB1_1_PWM_AXI_BASEADDR
-
-
-#ifdef __MICROBLAZE__	// ifdef and endif pair. define below used if found in xparamters.h
-#define CLK_FREQ XPAR_CPU_M_AXI_DP_FREQ_HZ
-#else
-//#define CLK_FREQ XPAR_PS7_CORTEXA9_0_CPU_CLK_FREQ_HZ // FCLK0 frequency not found in xparameters.h
-#define CLK_FREQ 1000000
-#endif
-
-#define PWM_PER 2
-
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-//UART
-//#define UART_DEVICE_ID		XPAR_XUARTPS_0_DEVICE_ID
+/*
+ * The following constants map to the XPAR parameters created in the
+ * xparameters.h file. They are defined here such that a user can easily
+ * change all the needed parameters in one place.
+ */
+#define UART_DEVICE_ID              XPAR_XUARTPS_0_DEVICE_ID
 
 /*
  * The following constant controls the length of the buffers to be sent
  * and received with the device, this constant must be 32 bytes or less since
  * only as much as FIFO size data can be sent or received in polled mode.
  */
-#define TEST_BUFFER_SIZE 16		// SEND INTERVALS OF 2 BYTES, mg
+#define TEST_BUFFER_SIZE 64		// SEND INTERVALS OF 2 BYTES, mg
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+
+int UartPsPolledExample(u16 DeviceId);
+
+/************************** Variable Definitions *****************************/
+
 XUartPs Uart_PS;		/* Instance of the UART Device */
+
+/*
+ * The following buffers are used in this example to send and receive data
+ * with the UART.
+ */
+static char SendBuffer[TEST_BUFFER_SIZE];	/* Buffer for Transmitting Data - change to char to debug */
 static u8 RecvBuffer[TEST_BUFFER_SIZE];	/* Buffer for Receiving Data */
 
-/************ Function Prototypes ************/
+// declare variables for TX part
+int ConsoleTXBufferLen = 0;
+int SendComplete = 1;
+int ConsoleTXBufferIndex = 0;
 
-void Initialize_bridges();
-void Run();
-void Cleanup();
 
-void EnableCaches();
-void DisableCaches();
+/*****************************************************************************/
+/**
+*
+* Main function to call the Uart Polled mode example.
+*
+* @param	None
+*
+* @return	XST_SUCCESS if succesful, otherwise XST_FAILURE
+*
+* @note		None
+*
+******************************************************************************/
 
-void drive_forward();
-void drive_back();
-void drive_left();
-void drive_right();
-void stop_moving();
-void kill_switch();
+int main(void)
+{
+	int Status;
 
-/************ Global Variables ************/
 
-PmodDHB1 pmodDHB1;
-PmodDHB2 pmodDHB2;
-XGpio input;
+	/*
+	 * Run the Uart_PS polled example , specify the the Device ID that is
+	 * generated in xparameters.h
+	 */
+	Status = UartPsPolledExample(UART_DEVICE_ID);
 
-/************ Function Definitions ************/
 
-int main(void) {
+	/*
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 */
 
-    // GPIO getting ready
+	if (Status != XST_SUCCESS) {
+		xil_printf("UART Polled Mode Example Test Failed\r\n");
+		return XST_FAILURE;
+	}
 
-	int button_data = 0;
-	int switch_data = 0;
+	xil_printf("Successfully ran UART Polled Mode Example Test\r\n");
+	return XST_SUCCESS;
+}
 
-	xil_printf("\nApplication started.... \r \n");
 
-	XGpio_Initialize(&input, XPAR_AXI_GPIO_0_DEVICE_ID);	//initialize input XGpio variable
+	/*
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 */
 
-	/******************************************************************************/
-	/******************************************************************************/
 
-	// init UART
+/**
+*
+* This function does a minimal test on the XUartPs device in polled mode.
+*
+* This function sends data and expects to receive the data thru the UART
+* using the local loopback mode.
+*
+*
+* @param	DeviceId is the unique device id from hardware build.
+*
+* @return	XST_SUCCESS if successful, XST_FAILURE if unsuccessful
+*
+* @note
+* This function polls the UART, it may hang if the hardware is not
+* working correctly.
+*
+****************************************************************************/
+int UartPsPolledExample(u16 DeviceId)
+{
+	int Status;
 	XUartPs_Config *Config;
+	unsigned int SentCount;
 	unsigned int ReceivedCount;
 	u16 Index;
 	u32 LoopCount = 0;
-	ReceivedCount = 0;
 
+
+	/*
+		 * --------------------------------------------------------------------------
+		 * --------------------------------------------------------------------------
+		 * --------------------------------------------------------------------------
+		 * --------------------------------------------------------------------------
+		 */
+
+	// GET SYSTEM READY
 	/*
 	 * Initialize the UART driver so that it's ready to use.
 	 * Look up the configuration in the config table, then initialize it.
-	*/
-		Config = XUartPs_LookupConfig(XPAR_XUARTPS_0_DEVICE_ID); // changed param 10/25
-		if (NULL == Config) {
-			return XST_FAILURE;
-		}
+	 */
+	Config = XUartPs_LookupConfig(DeviceId);
+	if (NULL == Config) {
+		return XST_FAILURE;
+	}
 
+	Status = XUartPs_CfgInitialize(&Uart_PS, Config, Config->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/* Check hardware build. */
+	Status = XUartPs_SelfTest(&Uart_PS);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 */
+
+	//--------------------------------------------------------------------------
+	// Set the operation mode
+	//--------------------------------------------------------------------------
 	XUartPs_SetBaudRate(&Uart_PS, 9600);	// changed value
+
+
+	/* Example used local loopback mode.
+	 * CHANGED TO NORMAL MODE , mg
+	 *  */
 	XUartPs_SetOperMode(&Uart_PS, XUARTPS_OPER_MODE_NORMAL);
 
-	// Init H-bridges
-	Initialize_bridges();
-
-   // polling data from switches/push buttons to control motors
-   while(1){
-
-  	// read input from channel 1 of axi_gpio_0 (buttons) and pass that data to "button_data" variable
-  	button_data = XGpio_DiscreteRead(&input, 1);
-  	switch_data = XGpio_DiscreteRead(&input, 2); // read switch input and store data in variable
-
-  	// receive buffer to control motors with XBEE
-  	XUartPs_Recv(&Uart_PS, &RecvBuffer[ReceivedCount], (TEST_BUFFER_SIZE - ReceivedCount));
-
-  	if(RecvBuffer[0] == '4'){
-  		drive_left();
-  	    usleep(6);
-  		}
-
-  	else if(button_data == 0b0010){
-  	  	drive_forward();
-  	  	usleep(6);
-  	  	}
-
-  	else if(button_data == 0b0100){
-  	  	 drive_back();
-  	  	 usleep(6);
-  	  	 }
-
-  	else if(button_data == 0b1000){
-  	  	 drive_right();
-  	  	 usleep(6);
-  	  	 }
-
-  	else if(switch_data == 0b1111){
-  	  	 kill_switch();
-  	  	 usleep(6);
-  	  	 }
-
-  	else{
-  		stop_moving();
-  	}
-
-     }
-
-   Cleanup();
-   return 0;
-}
-
-void Initialize_bridges() {
-   EnableCaches();
-
-   //init the 2 hbridges
-   DHB1_begin(&pmodDHB1, JD_GPIO_BASEADDR, JD_PWM_BASEADDR, CLK_FREQ, PWM_PER);
-   DHB2_begin(&pmodDHB2, JE_GPIO_BASEADDR, JE_PWM_BASEADDR, CLK_FREQ, PWM_PER);
-
-   // disable the 2 hbridges
-   DHB1_motorDisable(&pmodDHB1);
-   DHB2_motorDisable(&pmodDHB2);
-}
+	/*
+	 * Initialize the send buffer bytes with a pattern and zero out
+	 * the receive buffer.
+	 */
+	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
+		//SendBuffer[Index] = '0' + Index;  // this is getting : and ; for #s over 9
+		//SendBuffer[Index] = Index;
+		SendBuffer[Index] = Index;
+		RecvBuffer[Index] = 0;
+	}
 
 
-void Cleanup() {
-   DisableCaches();
-}
+	/*
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 */
 
-void drive_forward() { // case to move all 4 motors forward
-   DHB1_setMotorSpeeds(&pmodDHB1, 99, 99);		// JD = motor 1 top, motor 2 bottom
-   DHB2_setMotorSpeeds(&pmodDHB2, 99, 99);		// JE = motor 3 top, motor 4 bottom
+	//while(1){
+	/* Block sending the buffer. */
+	ConsoleTXBufferLen = XUartPs_Send(&Uart_PS, SendBuffer, TEST_BUFFER_SIZE);  // get length of send buffer
+	if (ConsoleTXBufferLen != TEST_BUFFER_SIZE) {
+		return XST_FAILURE;
+	}
 
-   DHB1_motorDisable(&pmodDHB1); // Disable PWM before changing direction
-   DHB2_motorDisable(&pmodDHB2); // short circuit possible otherwise
-   usleep(6);
+	//Frankenstein version of send_string_buffer function from Dr. Lusher
+	ConsoleTXBufferIndex = 0;
+	SendComplete = 0;
 
-   DHB1_setDirs(&pmodDHB1, 0, 0); // Set DIR A and B motors forward (left side)
-   DHB2_setDirs(&pmodDHB2, 1, 1); // Set DIR C and D motors forward (right side)
+	//while (ConsoleTXBufferIndex <  ConsoleTXBufferLen)  // comment out 11/15
+	while (1)
+		{
+			XUartPs_Send(&Uart_PS, (u8 *)&SendBuffer[ConsoleTXBufferIndex++], 1);
+			//ConsoleTXBufferIndex++;
+		}
 
-   usleep(6);
-   DHB1_motorEnable(&pmodDHB1);	// Motors A and B
-   DHB2_motorEnable(&pmodDHB2);	// Motors C and D
+	/*
+	 * Wait while the UART is sending the data so that we are guaranteed
+	 * to get the data the 1st time we call receive, otherwise this function
+	 * may enter receive before the data has arrived
+	 */
+	while (XUartPs_IsSending(&Uart_PS)) {	// check if device is still sending data, if so delay by incrementing loopcount+
+		LoopCount++;
+	}
 
-   xil_printf("\nRunning 4 motors forward\n");
-   usleep(6);
-   DHB1_motorDisable(&pmodDHB1);
-   DHB2_motorDisable(&pmodDHB2);
-}
 
-void drive_back() { // case to move all 4 motors backward
-   DHB1_setMotorSpeeds(&pmodDHB1, 99, 99);		// JD = motor 1 top, motor 2 bottom
-   DHB2_setMotorSpeeds(&pmodDHB2, 99, 99);		// JE = motor 3 top, motor 4 bottom
+	/*
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 */
 
-   DHB1_motorDisable(&pmodDHB1); // Disable PWM before changing direction
-   DHB2_motorDisable(&pmodDHB2); // short circuit possible otherwise
-   usleep(6);
-   DHB1_setDirs(&pmodDHB1, 1, 1); // Set DIR A and B motors backwards (left side)
-   DHB2_setDirs(&pmodDHB2, 0, 0); // Set DIR C and D motors backwards (right side)
-   usleep(6);
+	/* Block receiving the buffer. */
+	ReceivedCount = 0;
+	//while (ReceivedCount < TEST_BUFFER_SIZE) {  // CHANGED DURING TESTING 8/24
+	while (1) {
+		XUartPs_Recv(&Uart_PS, &RecvBuffer[ReceivedCount], (TEST_BUFFER_SIZE - ReceivedCount));
+	}
 
-   DHB1_motorEnable(&pmodDHB1);	// Motors A and B
-   DHB2_motorEnable(&pmodDHB2);	// Motors C and D
+	//Frankenstein version of receive data functions from Dr. Lusher
+	//while (XUartPs_IsReceiveData(Uart_PS.Config.BaseAddress))
+		//{
+		// DOUBLE CHECK THIS --- 10/15 mg
+			u8 buffer[32];
+			XUartPs_Recv(&Uart_PS, buffer, 1);
+	//	}
 
-   xil_printf("\nRunning 4 motors backward\n");
-   usleep(6);
-   DHB1_motorDisable(&pmodDHB1);
-   DHB2_motorDisable(&pmodDHB2);
-}
+	/*
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 * --------------------------------------------------------------------------
+	 */
 
-/*move C and D motors at max speed forward to turn left,
- * AND also move A and B at MAX speed but in opposite direction backward
- * to help with torque and turn better	*/
-void drive_left() {
-   DHB1_setMotorSpeeds(&pmodDHB1, 99, 99);		// JD = motor 1 top, motor 2 bottom
-   DHB2_setMotorSpeeds(&pmodDHB2, 99, 99);		// JE = motor 3 top, motor 4 bottom
+	// should not need this function below only as a check
 
-   DHB1_motorDisable(&pmodDHB1); // Disable PWM before changing direction
-   DHB2_motorDisable(&pmodDHB2); // short circuit possible otherwise
-   usleep(6);
-   DHB1_setDirs(&pmodDHB1, 1, 1); // Set DIR A and B motors back (left side)
-   DHB2_setDirs(&pmodDHB2, 1, 1); // Set DIR C and D motors forward (right side)
-   usleep(6);
+	/*
+	 * Check the receive buffer against the send buffer and verify the
+	 * data was correctly received
+	 */
 
-   DHB1_motorEnable(&pmodDHB1);	// Motors A and B
-   DHB2_motorEnable(&pmodDHB2);	// Motors C and D
+	/*
+	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
+		if (SendBuffer[Index] != RecvBuffer[Index]) {
+			return XST_FAILURE;
+		}
+	}
+	*/
 
-   xil_printf("\nRunning 4 motors left\n");
-   usleep(6);
-   DHB1_motorDisable(&pmodDHB1);
-   DHB2_motorDisable(&pmodDHB2);
-}
+	/* Restore to normal mode. */
+	//XUartPs_SetOperMode(&Uart_PS, XUARTPS_OPER_MODE_NORMAL);
 
-/*move A and B motors forward at max speed to turn right,
- * AND still move C and D backwards at max speed to help with torque
- * and turn better	*/
-void drive_right() {
-   DHB1_setMotorSpeeds(&pmodDHB1, 99, 99);		// JD = motor 1 top, motor 2 bottom
-   DHB2_setMotorSpeeds(&pmodDHB2, 99, 99);		// JE = motor 3 top, motor 4 bottom
-
-   DHB1_motorDisable(&pmodDHB1); // Disable PWM before changing direction
-   DHB2_motorDisable(&pmodDHB2); // short circuit possible otherwise
-   usleep(6);
-
-   DHB1_setDirs(&pmodDHB1, 0, 0); // Set DIR A and B motors forward (left side)
-   DHB2_setDirs(&pmodDHB2, 0, 0); // Set DIR C and D motors back (right side)
-
-   usleep(6);
-
-   DHB1_motorEnable(&pmodDHB1);	// Motors A and B
-   DHB2_motorEnable(&pmodDHB2);	// Motors C and D
-
-   xil_printf("\nRunning 4 motors right\n");
-   usleep(6);
-   DHB1_motorDisable(&pmodDHB1);
-   DHB2_motorDisable(&pmodDHB2);
-}
-
-void stop_moving(){
-	usleep(6);
-	xil_printf("\nMOTORS ON\n");
-	DHB1_motorDisable(&pmodDHB1);
-	DHB2_motorDisable(&pmodDHB2);
-}
-
-void kill_switch(){
-	usleep(6);
-	DHB1_motorDisable(&pmodDHB1);
-	DHB2_motorDisable(&pmodDHB2);
-	xil_printf("\nALL MOTORS DISABLED\n");
-	sleep(45);	// virtual kill switch ~ kill motors for 45 seconds only, then run stop_moving
-}
-
-void EnableCaches() {
-   Xil_DCacheEnable();
-}
-
-void DisableCaches() {
-   Xil_DCacheDisable();
+	return XST_SUCCESS;
 }
